d020c03042e5623590cb6a9d7ec413613e3279a7
marknesh <markmunene.nesh@gmail.com>
2019-12-07 19:43:24 +0300

    add form tags and add class form control

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$("h6.iconDesign").click(function(){
+    $("p.hideDesign").toggle()
+})
+$("h6.iconDevelopment").click(function(){
+    $("p.hideDevelopment").toggle()
+})
+$("h6.iconProduct").click(function(){
+    $("p.hideProduct").toggle()
+})
