b7d0c2de44c4dedcfbcad8369c8918068479098c
marknesh <markmunene.nesh@gmail.com>
2019-12-09 09:44:56 +0300

    add image in readme

--- js/script.js
+++ js/script.js
@@ -2,12 +2,29 @@
 
 $("img.iconDesign").click(function(){
     $("p.hideDesign").toggle()
+    $("img.iconDesign").hide()
+
+})
+
+$("p.hideDesign").click(function(){
+    $("img.iconDesign").show();
+    $("p.hideDesign").hide()
 })
 $("img.iconDevelopment").click(function(){
     $("p.hideDevelopment").toggle()
+    $("img.iconDevelopment").hide()
+})
+$("p.hideDevelopment").click(function(){
+    $("img.iconDevelopment").show();
+    $("p.hideDevelopment").hide()
 })
 $("img.iconProduct").click(function(){
     $("p.hideProduct").toggle();
+    $("img.iconProduct").hide()
+})
+$("p.hideProduct").click(function(){
+    $("img.iconProduct").show();
+    $("p.hideProduct").hide()
 })
 
     $("form.news").submit(function(){
